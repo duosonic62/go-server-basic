06804b9e3f7ea11f64bf880aa0b709dad233e7c6
Sekiguchi Riku <l.life.style.of.sustainability@gmail.com>
2019-07-22 21:43:30 +0900

    add simple server

--- src/main/main.go
+++ src/main/main.go
@@ -1,46 +1,15 @@
 package main
 
 import (
-	"html/template"
 	"net/http"
 )
 
 func main() {
-	mux := http.NewServeMux()
-	files := http.FileServer(http.Dir("/public"))
-	mux.Handle("/static/", http.StripPrefix("/static/", files))
-
-	mux.HandleFunc("/", index)
-	mux.HandleFunc("/err", err)
-
-	mux.HandleFunc("/login", login)
-	mux.HandleFunc("/logout", logout)
-	mux.HandleFunc("/signup", signup)
-	mux.HandleFunc("/authenticate", authenticate)
-
-	mux.HandleFunc("/thread/new", newThread)
-	mux.HandleFunc("/thread/crate", createThread)
-	mux.HandleFunc("/thread/post", postThread)
-	mux.HandleFunc("/thread/read", readThread)
 
 	server := &http.Server{
-		Addr:    "0.0.0.0:8080",
-		Handler: mux,
+		Addr:    "127.0.0.1:8080",
+		Handler: nil,
 	}
 
 	server.ListenAndServe()
-}
-
-func index(w http.ResponseWriter, r *http.Request) {
-	files := []string{
-		"templates/layout.html",
-		"templates/navbar.html",
-		"templates/index.html",
-	}
-	templates := template.Must(template.ParseFiles(files...))
-	threads, err := data.Threads()
-	if err == nil {
-		templates.ExecuteTemplate(w, "layout", threads)
-	}
-
 }
