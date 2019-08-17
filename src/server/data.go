0dbbd63ae7c97e5c31b4013fd5e7d97ea6d91ef6
Sekiguchi Riku <l.life.style.of.sustainability@gmail.com>
2019-08-17 11:34:14 +0900

    add crud api

---
+++ src/server/data.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var Db *sql.DB
+
+func init() {
+	var err error
+	Db, err = sql.Open("mysql", "gwp_dev_user:gwp@/gwp")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func retrieve(id int) (post Post, err error) {
+	post = Post{}
+	err = Db.QueryRow("select id, content, author from posts where id = ?", id).Scan(&post.Id, &post.Content, &post.Author)
+	return
+}
+
+func (post *Post) create() (err error) {
+	statement, err := Db.Prepare("insert into posts (content, author) values (?, ?)")
+	_, err = statement.Exec(post.Content, post.Author)
+	err = Db.QueryRow("SELECT LAST_INSERT_ID()").Scan(&post.Id)
+	if err != nil {
+		panic(err)
+	}
+
+	return
+}
+
+func (post *Post) update() (err error) {
+	_, err = Db.Exec("update posts set content = ?, author = ? where id = ?", post.Content, post.Author, post.Id)
+	return
+}
+
+func (post *Post) delete() (err error) {
+	_, err = Db.Exec("delete from posts where id = ?", post.Id)
+	return
+}
